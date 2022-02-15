33357ad56154f5908c93fc3ec1bba3cadaeeb56d
Michelle Ferris <jmichellelim@yahoo.com>
2022-02-14 23:13:48 -0500

    init

---
+++ src/main/java/org/launchcode/hellospringdemo/HelloSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.hellospringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/hellospringdemo/HelloSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.hellospringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
